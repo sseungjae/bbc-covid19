9fc49e4659b343caae38d05a50b80a6291736f72
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-11-17 21:18:38 +0900

    bounfingRect check
    innerHeight
    step.dataset.index check

--- main.js
+++ main.js
@@ -9,4 +9,19 @@
         graphicElement[i].dataset.index = i;
     }
 
+    window.addEventListener('scroll', ()=> {
+        let step;
+        let boundingRect;
+
+        for(let i = 0; i < stepElement.length; i ++){
+            step = stepElement[i];
+            boundingRect = step.getBoundingClientRect();
+
+            if(boundingRect.top > window.innerHeight * 0.1 &&
+                boundingRect.top < window.innerHeight * 0.8){
+                    console.log(step.dataset.index);
+                }
+        }
+    });
+
 })();
