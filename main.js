89b4fee37871b28deb90620a4679c9dd12c3664a
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-11-18 00:33:15 +0900

    code fixed
    simply

--- main.js
+++ main.js
@@ -2,12 +2,22 @@
 
     const stepElement = document.querySelectorAll('.step');
     const graphicElement = document.querySelectorAll('.graphic-item');
+    let currentItem = graphicElement[0]; //현재 활성화된 visible 클래스가 붙은  .graphic-item을 나타냄
 
     for(let i = 0 ; i < stepElement.length; i++){
         // stepElement[i].setAttribute('data-index', i);
         stepElement[i].dataset.index = i;
         graphicElement[i].dataset.index = i;
     }
+
+    function activate(){
+        currentItem.classList.add('visible');
+    }
+
+    function inactivate(){
+        currentItem.classList.remove('visible');
+    }
+
 
     window.addEventListener('scroll', ()=> {
         let step;
@@ -19,9 +29,14 @@
 
             if(boundingRect.top > window.innerHeight * 0.1 &&
                 boundingRect.top < window.innerHeight * 0.8){
-                    console.log(step.dataset.index);
-                }
+                
+                inactivate(); 
+                currentItem = graphicElement[step.dataset.index];
+                activate();
+            }
         }
     });
+
+    activate();
 
 })();
