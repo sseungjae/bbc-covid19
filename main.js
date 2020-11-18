43f1cfb1e7b6dcfec5ccae83390b748c5750dac0
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-11-18 22:41:34 +0900

    simply loop
    loop code

--- main.js
+++ main.js
@@ -3,8 +3,14 @@
     const stepElement = document.querySelectorAll('.step');
     const graphicElement = document.querySelectorAll('.graphic-item');
     let currentItem = graphicElement[0]; //현재 활성화된 visible 클래스가 붙은  .graphic-item을 나타냄
+    let ioIndex;
+
+    const io = new IntersectionObserver((entries, observer) => {
+        ioIndex = entries[0].target.dataset.index * 1;
+    });
 
     for(let i = 0 ; i < stepElement.length; i++){
+        io.observe(stepElement[i]);
         // stepElement[i].setAttribute('data-index', i);
         stepElement[i].dataset.index = i;
         graphicElement[i].dataset.index = i;
@@ -23,9 +29,11 @@
         let step;
         let boundingRect;
 
-        for(let i = 0; i < stepElement.length; i ++){
+        //for(let i = 0; i < stepElement.length; i ++){
+        for(let i = ioIndex - 1; i < ioIndex + 2; i ++){
             step = stepElement[i];
-            boundingRect = step.getBoundingClientRect();
+            if(!step) continue;
+            boundingRect = step.getBoundingClientRect(); 
 
             if(boundingRect.top > window.innerHeight * 0.1 &&
                 boundingRect.top < window.innerHeight * 0.8){
@@ -35,6 +43,7 @@
                 activate();
             }
         }
+        
     });
 
     activate();
